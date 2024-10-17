4b5947e1a83c50e778d3cc894456982be89c9ef8
LilianXeyman <elisa0101232478@gmail.com>
2024-10-17 15:57:12 +0100

    Actualizacion del popUp confirmar compra
    Si le das al botón de no te vuelve a llevar a la tienda.
    No se como hacer para que te cobré en el botón del si.
    Ahora mismo me esta cobrando antes de que salga el mensaje de confirmar tienda.

--- Assets/Scripts/Wallet.cs
+++ Assets/Scripts/Wallet.cs
@@ -20,7 +20,6 @@ public class Wallet : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
         saldo = Random.Range(450f, 950f);
         etiquetaSaldo.text = saldo.ToString("000.00") + "€";
     }
@@ -29,12 +28,26 @@ public class Wallet : MonoBehaviour
     {
         if (saldo > precio)
         {
-            saldo -= precio;
-            etiquetaSaldo.text = saldo.ToString("000.00") + "€";
+           confirmarCompra.SetActive(true);
+           saldo -= precio;
+           etiquetaSaldo.text = saldo.ToString("000.00") + "€";
+
         }
         else 
         {
             sinSaldo.SetActive(true);
         }
+    }
+    public void SinSaldo()
+    {
+        sinSaldo.SetActive(false);
+    }
+    public void SiCompra()
+    { 
+        confirmarCompra.SetActive(true);
+    }
+    public void NoCompra()
+    {
+        confirmarCompra.SetActive(false);
     }
 }
